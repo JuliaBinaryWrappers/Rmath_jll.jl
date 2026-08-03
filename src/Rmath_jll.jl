a7f42b8109da30acb298a7ba8f56d58840fab4d9
jlbuild <juliabuildbot@gmail.com>
2026-08-03 17:41:12 +0000

    Rmath_jll build 0.5.2+0

--- src/Rmath_jll.jl
+++ src/Rmath_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Rmath")
-JLLWrappers.@generate_main_file("Rmath", UUID("f50d1b31-88e8-58de-be2c-1cc44531875f"))
+JLLWrappers.@generate_main_file("Rmath", Base.UUID("f50d1b31-88e8-58de-be2c-1cc44531875f"))
 end  # module Rmath_jll
